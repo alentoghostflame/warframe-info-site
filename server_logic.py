8924defc57a829ff52dfb8fd738feaa1737cbaa4
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2019-09-27 22:01:08 -0500

    Even better hello world

--- server_logic.py
+++ server_logic.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 website = Flask(__name__)
 
 
-@website.route("/")
-@website.route("/index")
-def index():
-    return render_template("base.html")
+@website.route("/", strict_slashes=False)
+def homepage():
+    return render_template("homepage.html")
+
+
+@website.route("/<path:path>")
+def catch_all(path):
+    return redirect(url_for("homepage"))
