62868521f737bce6fa2ca9ec96ec021137940c5f
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2019-10-03 14:15:36 -0500

    Added specific CSS for the warframe list, added few other things

--- class_logic.py
+++ class_logic.py
@@ -1,3 +1,4 @@
+from os import listdir
 import yaml
 
 
@@ -13,13 +14,48 @@ DEFAULT_BASIC_OBJECT: dict = {
 }
 
 
-class BasicObject:
+class ObjectStorage:
     def __init__(self):
-        self.data: dict = DEFAULT_BASIC_OBJECT.copy()
+        self.warframe_list = WarframeList()
 
-    def load(self, file_object):
-        pass
+    def load_all(self):
+        self.warframe_load_all()
 
-    def save(self, file_name):
+    def warframe_load_all(self):
+        self.warframe_list.load_all()
+
+    def warframe_load(self, file_path: str):
+        self.warframe_list.load(file_path)
+
+
+class WarframeList:
+    def __init__(self):
+        self.raw: list = []
+        self.names: list = []
+
+    def load_all(self):
+        file_list = listdir("data/warframes/")
+        for file in file_list:
+            self.load("data/warframes/{}".format(file))
+
+    def load(self, file_path: str):
+        warframe = BasicObject()
+        warframe.load_from_file(file_path)
+        self.raw.append(warframe)
+        self.names.append([warframe.get("name", "Name Not Found!"), file_path.split("/")[-1]])
+        # self.names.append(warframe.get("name", "Name Not Found!"))
+        # self.file_names.append(file_path.split("/")[-1])
+
+
+class BasicObject(dict):
+    def __init__(self):
+        dict.__init__(self)
+        self.update(DEFAULT_BASIC_OBJECT.copy())
+
+    def load_from_file(self, file_path: str):
+        file = open(file_path, "r")
+        self.update(yaml.full_load(file))
+
+    def save_to_file(self, file_name: str):
         file = open(file_name, "w")
-        yaml.dump(self.data, file, default_flow_style=None)
+        yaml.dump(self, file, default_flow_style=None)
--- server_logic.py
+++ server_logic.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, redirect, url_for
+from class_logic import ObjectStorage
 
 
 website = Flask(__name__)
+storage = ObjectStorage()
+storage.load_all()
 
 
 @website.route("/", strict_slashes=False)
@@ -9,9 +12,18 @@ def homepage():
     return render_template("homepage.html")
 
 
+@website.route("/warframes/<path:path>", strict_slashes=False)
+def warframes(path):
+    print("Warframes: User tried to go to /warframes/{}".format(path))
+    if path.lower in storage.warframe_list.names:
+        return "lol hi there."
+    else:
+        return redirect(url_for("warframe_list"))
+
+
 @website.route("/warframes", strict_slashes=False)
 def warframe_list():
-    return render_template("warframe_list.html")
+    return render_template("warframe_list.html", warframe_names=storage.warframe_list.names)
 
 
 @website.route("/<path:path>", strict_slashes=False)
