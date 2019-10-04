94a8dfd868b3283518b2d81ec1144f6b8c4fc901
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2019-10-03 22:30:49 -0500

    Added beginning of the DropTableParser

--- class_logic.py
+++ class_logic.py
@@ -1,3 +1,4 @@
+from html.parser import HTMLParser
 from os import listdir
 import yaml
 
@@ -28,6 +29,84 @@ class ObjectStorage:
         self.warframe_list.load(file_path)
 
 
+class DropTableParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.storage: dict = {}
+        self.missions: set = set()
+        self.relics: set = set()
+
+        self.selected_type: str = ""
+
+        self.mission_location: str = ""
+        self.mission_rotation: str = "default"
+        self.item_name: str = ""
+        self.drop_chance = ""
+        self.temp_tag: str = ""
+        self.temp_attribute: tuple = ()
+
+    def read_file(self, file_path: str):
+        self.feed(open(file_path, "r").read())
+
+    def handle_starttag(self, tag, attrs):
+        # print("Start tag: {}   Attributes: {}".format(tag, attrs))
+        if tag == "h3":
+            if attrs == [("id", "missionRewards")] or attrs == [("id", "keyRewards")] or \
+                    attrs == [("id", "transientRewards")]:
+                self.selected_type = "mission"
+            else:
+                self.selected_type = "other"
+
+        self.temp_tag = tag
+        self.temp_attribute = attrs
+
+    def handle_endtag(self, tag):
+        # print("End tag: {}".format(tag))
+        pass
+
+    def handle_data(self, data):
+        # print("Data: {}".format(data))
+        if data.rstrip():
+            if self.selected_type == "mission":
+                if self.temp_tag == "th":
+                    if "Rotation" in data:
+                        # Rotation in a mission; 1, 2, and 3 typically.
+                        self.mission_rotation = data
+                        self.storage[self.mission_location][self.mission_rotation] = []
+                    else:
+                        # Should be the name or location of the mission.
+                        self.mission_location = data
+                        self.missions.add(self.mission_location)
+                        self.storage[self.mission_location] = {}
+                        self.storage[self.mission_location]["default"] = []
+                        self.mission_rotation = "default"
+                elif self.temp_tag == "td":
+                    if "%" in data:
+                        # It's the drop chance of an item.
+                        self.drop_chance = data
+                    else:
+                        # Should be the name of the item.
+                        self.item_name = data
+                if self.item_name and self.drop_chance:
+                    self.storage[self.mission_location][self.mission_rotation].append({"item_name": self.item_name,
+                                                                                       "drop_chance": self.drop_chance})
+                    self.item_name = ""
+                    self.drop_chance = ""
+
+    def error(self, message):
+        print("ERROR:", message)
+
+
+"""
+import yaml
+import class_logic
+test = class_logic.DropTableParser()
+test.read_file("Unminified_Warframe_DropTables.html")
+yaml.dump(test.storage, open("Test.yaml", "w"), default_flow_style=None)
+exit()
+"""
+
+
 class WarframeList:
     def __init__(self):
         self.raw: list = []
@@ -44,7 +123,6 @@ class WarframeList:
         warframe = BasicObject()
         warframe.load_from_file(file_path)
         self.raw.append(warframe)
-        # self.names.append([warframe.get("name", "Name Not Found!"), file_path.split("/")[-1].rsplit(".")[0]])
         self.names.append(warframe.get("name", "Name not found!"))
         self.file_names.append(file_path.split("/")[-1].rsplit(".", 1)[0])
         self.warframes[file_path.split("/")[-1].rsplit(".", 1)[0]] = warframe
